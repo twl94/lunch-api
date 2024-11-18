3abdcc83ede8d020d7861593261c45ad88962a29
twl94 <133763368+twl94@users.noreply.github.com>
2024-11-18 20:27:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fetch = require('node-fetch')
 var cors = require('cors')
 app.use(cors())
 
-app.get('/', (req, res) => { res.send(`<script>window.location.href = "https://github.com/Hello50511/lunch-api"</script>`) })
+app.get('/', (req, res) => { res.send(`<script>window.location.href = "https://github.com/twl94/lunch-api"</script>`) })
 
 app.get('/school', (req, res) => {
     let schoolName = req.query.name;
@@ -14,6 +14,7 @@ app.get('/school', (req, res) => {
     fetch(url).then(res => res.json()).then(json => {
         let schools = []
 
+        if (!schoolName) return res.send({ error: "name 파라미터 값이 제공되지 않았습니다." })
         for (var i = 0; i < json['schoolInfo'][1]["row"].length; i++) {
             let schoolData = json['schoolInfo'][1]["row"][i];
 
@@ -31,15 +32,22 @@ app.get('/school', (req, res) => {
 
         res.send(schools)
 
+    }).catch(e=> {
+        res.send({error:e})
+
     })
 })
 
 app.get('/lunch', (req, res) => {
     let regionCode = req.query.region;
     let schoolCode = req.query.code;
+    let date = req.query.date;
 
-    var today = new Date().toISOString().substring(0,10).replace(/-/g,'');
-    let url = `https://open.neis.go.kr/hub/mealServiceDietInfo?Type=json&pSize=100&ATPT_OFCDC_SC_CODE=${regionCode}&SD_SCHUL_CODE=${schoolCode}&MMEAL_SC_CODE=2&MLSV_YMD=${today}`
+    if (!date) return res.send({ error: "date 파라미터 값이 제공되지 않았습니다." })
+    if (!regionCode) return res.send({ error: "region 파라미터 값이 제공되지 않았습니다." })
+    if (!schoolCode) return res.send({ error: "code 파라미터 값이 제공되지 않았습니다." })
+
+    let url = `https://open.neis.go.kr/hub/mealServiceDietInfo?Type=json&pSize=100&ATPT_OFCDC_SC_CODE=${regionCode}&SD_SCHUL_CODE=${schoolCode}&MMEAL_SC_CODE=2&MLSV_YMD=${date}`
 
     fetch(url).then(res => res.json()).then(json => {
         console.log(json)
@@ -54,6 +62,8 @@ app.get('/lunch', (req, res) => {
         }
         
         res.send(data)
+    }).catch(er => {
+        return res.send({ error: `급식 정보를 파싱중 예기치 못한 오류가 발생하였습니다. (${er})` })
     })
 })
 
